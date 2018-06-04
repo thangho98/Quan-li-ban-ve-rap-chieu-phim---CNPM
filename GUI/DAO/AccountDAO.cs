5011e189544e5d3e4f52b5bcdb10ba4d889c34ab
doublefeel98 <thanglong2098@gmail.com>
2018-06-04 10:22:30 +0700

    add password encryption function on frmLogin

--- GUI/DAO/AccountDAO.cs
+++ GUI/DAO/AccountDAO.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Security.Cryptography;//thư viện để mã hóa mật khẩu
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,10 +13,33 @@ namespace GUI.DAO
     {
         private AccountDAO() { }
 
+        private static string PasswordEncryption(string password)
+        {
+            //tính năng bảo mật cho việc đăng nhập
+            byte[] temp = ASCIIEncoding.ASCII.GetBytes(password);//chuyển qua mảng kiểu byte từ một chuỗi
+            byte[] hasData = new MD5CryptoServiceProvider().ComputeHash(temp);
+            //tạo ra bảng has(bảng băm) chứa các mảng byte
+            //từ mật khẩu được mã hóa thành mảng băm
+
+            string hasPass = "";
+
+            foreach (byte item in hasData)
+            {
+                hasPass += item;
+            }
+
+            //tính năng mã hóa nâng cao bằng việc đảo ngược mật khẩu
+            char[] arr = hasPass.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+
         public static int Login(string userName, string passWord)
         {
-			string query = "USP_Login @userName , @passWord";
-			DataTable result = DataProvider.ExecuteQuery(query, new object[] { userName, passWord  /*list*/});
+            string pass = PasswordEncryption(passWord);
+
+            string query = "USP_Login @userName , @passWord";
+			DataTable result = DataProvider.ExecuteQuery(query, new object[] { userName, pass });
 
             if (result == null)
                 return -1;
@@ -25,10 +49,14 @@ namespace GUI.DAO
                 return 0;
             
         }
-
-        public static bool UpdateAccountPassword(string username, string pass, string newPass)
+        
+        public static bool UpdateAccountPassword(string userName, string passWord, string newPassWord)
         {
-            int result = DataProvider.ExecuteNonQuery("EXEC USP_UpdateAccount @username , @pass , @newPass", new object[] { username, pass, newPass });
+
+            string oldPass = PasswordEncryption(passWord);
+            string newPass = PasswordEncryption(newPassWord);
+
+            int result = DataProvider.ExecuteNonQuery("EXEC USP_UpdateAccount @username , @pass , @newPass", new object[] { userName, oldPass, newPass });
 
             return result > 0;
         }
